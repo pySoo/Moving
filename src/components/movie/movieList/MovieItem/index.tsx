aa73803afddef9968b07636eee31f308bc15ef2b
pySoo <soopy368@gmail.com>
2023-12-09 23:19:47 +0900

    [feat] : modal 여는 로직을 navigate로만 처리하도록 수정

--- src/components/movie/movieList/MovieItem/index.tsx
+++ src/components/movie/movieList/MovieItem/index.tsx
@@ -2,8 +2,6 @@ import { useNavigate } from 'react-router-dom';
 
 import GradientTag from '@/components/common/GradientTag';
 import MoviePoster from '@/components/movie/MoviePoster';
-import { useModal } from '@/hooks/useModal';
-import { ModalType } from '@/types/modalTypes';
 import { Movie } from '@/types/movieTypes';
 
 import styles from './styles.module.scss';
@@ -15,12 +13,8 @@ interface MovieListItemProps {
 export default function MovieListItem({ movie }: MovieListItemProps) {
   const navigate = useNavigate();
 
-  const { openModal } = useModal();
-
   const handleOpenMovieModal = () => {
     navigate(`/?movieId=${movie.imdbID}`);
-
-    openModal({ type: ModalType.MOVIE_DETAIL });
   };
 
   return (
